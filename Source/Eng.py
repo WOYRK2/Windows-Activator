d2701c258ce39e9d600468fda89d703a9bb24764
WOYRK2 <dssk1235@gmail.com>
2026-02-24 20:11:22 +0300

    Add files via upload

---
+++ Source/Eng.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import os
+import sys
+import json
+from functools import partial
+
+#Окно
+window = tk.Tk()
+window.title(r"Рус/Welcome)")
+window.geometry('1920x1080')
+#НАстройка
+
+text_Label = tk.Label(window, text='Select ur windows')
+text_Label.pack(pady=10)
+
+def ActWin(Win):
+    print('-----------------------------')
+    Key = None
+
+    with open('Config.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+        for D2 in data['windows']:
+            if D2 == Win:
+                NameWin = D2
+                KeyWint = data['windows'][D2]
+                print(f'{NameWin} - {KeyWint}')
+
+                os.system(f'slmgr /ipk {KeyWint}')
+                os.system('slmgr /skms kms.digiboy.ir')
+                os.system('slmgr /ato')
+
+def CreateButton():
+    with open(r'Config.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+        for Data2 in data['windows']:
+            d1 = data['windows'][Data2]
+            print(f'{Data2} - {d1}')
+
+            button = tk.Button(window, text=Data2, command=lambda n=Data2: ActWin(n))
+            button.pack(pady=10)
+
+
+CreateButton()
+window.mainloop()
---
+++ Source/main.py
@@ -0,0 +1,68 @@
+import glob
+import json
+import tkinter
+import os
+import sys
+
+with open('Config.json', 'r', encoding='utf-8') as f:
+    pass
+    data = json.load(f)
+
+    if data['Lang'] is None:
+        #Настройка
+        window = tkinter.Tk()
+        window.title('Select ur language')
+        window.geometry('500x300')
+
+        def selectLang(getLang):
+            data['Lang'] = getLang
+            json.dump(data, open('Config.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+            window.destroy()
+
+        Text = tkinter.Label(window, text='Pls select ur language')
+        Text.pack(pady=10)
+
+        ButtonRus = tkinter.Button(window, text='Russ', command=lambda: selectLang('Rus'), width=10, height=5)
+        ButtonRus.pack(pady=10)
+
+        ButtonEng = tkinter.Button(window, text='English', command=lambda: selectLang('Eng'), width=10, height=5)
+        ButtonEng.pack(pady=10)
+
+        #Заруск окан
+        window.mainloop()
+
+with open('Config.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+    if data['Lang'] == 'Rus':
+        #Папки
+        Folders = glob.glob('**/*.exe', recursive=True)
+        print(Folders)
+
+        flag = False
+        for path in Folders:
+            print(path)
+            if path == r'Rus.exe':
+                os.startfile(r'Rus.exe')
+                flag = True
+        else:
+            if not flag:
+                from tkinter import messagebox
+                messagebox.showwarning('Ошибка', 'Не был найден exe файл с навзанием Rus.exe. Посмотрите в корневой папке программы есть ли такой файл и запустите его самостоятельно.')
+
+    else:
+        # Папки
+        Folders = glob.glob('**/*.exe', recursive=True)
+        print(Folders)
+
+        flag = False
+        for path in Folders:
+            print(path)
+            if path == r'Eng.exe':
+                os.startfile(r'Eng.exe')
+                flag = True
+        else:
+            if not flag:
+                from tkinter import messagebox
+                messagebox.showwarning('Error', 'No Eng.exe file was found. Check the programs root folder for the file and run it yourself.')
+
